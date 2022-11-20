968752cff1170582885570900c06d7782f7f844a
Wenjing Li <wjingli@umich.edu>
2022-11-20 02:53:29 -0500

    add new version of vignettes

---
+++ R/Square.R
@@ -0,0 +1,16 @@
+#'Linear Regression
+#'
+#'Gets the square of a number
+#'
+#'@param x input value
+#'
+#'@return the square of x
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+square = function(x) {
+  return (x^2)
+}
